5cfd743bff41616a89615a091394d438aa9fee74
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-28 11:51:08 +0700

    Meta tweaks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const isUtf8 = require('is-utf8');
 
 module.exports = buffer => {
 	if (!Buffer.isBuffer(buffer)) {
-		throw new TypeError('Expected a Buffer, got ' + typeof buffer);
+		throw new TypeError(`Expected a Buffer, got ${typeof buffer}`);
 	}
 
 	if (buffer[0] === 0xEF && buffer[1] === 0xBB && buffer[2] === 0xBF && isUtf8(buffer)) {
